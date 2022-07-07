7199a0ceab994c715a047b6093ddd0a136d13f4a
Summer-Lo <hin080199@gmail.com>
2022-07-07 17:30:40 +0800

    Update

--- autoStart_setup.bash
+++ autoStart_setup.bash
@@ -4,4 +4,5 @@ sudo cp LXinput-TM5022.desktop /home/pi/.config/autostart/
 sudo chmod 777 /home/pi/.config/autostart/LXinput-TM5022.desktop
 sudo cp TM5022.sh /home/pi
 sudo chmod 777 /home/pi/TM5022.sh
-
+sudo cp TM5022_vlc.sh /home/pi
+sudo chmod 777 /home/pi/TM5022_vlc.sh
